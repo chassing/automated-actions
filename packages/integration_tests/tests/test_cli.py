ee63b098dd31bc6d18dbbac4da63c3cc5000cd1d
Christian Assing <cassing@redhat.com>
2025-05-22 11:54:50 +0200

    :white_check_mark: [integration-tests] fix cli tests and show output in case of errors for easier debugging (#113)

--- packages/integration_tests/tests/test_cli.py
+++ packages/integration_tests/tests/test_cli.py
@@ -25,36 +25,36 @@ def cli() -> AACli:
 def test_cli_help(cli: AACli) -> None:
     result = cli("json", "--help")
 
-    assert "Usage:" in result.stdout
+    assert "Usage:" in result.stdout, result.stdout
 
 
 def test_cli_me(cli: AACli) -> None:
     result = json.loads(cli("json", "me").stdout)
 
-    assert "allowed_actions" in result
-    assert "name" in result
+    assert "allowed_actions" in result, result
+    assert "name" in result, result
 
 
 def test_cli_output_yaml(cli: AACli) -> None:
     result = yaml.safe_load(cli("yaml", "me").stdout)
 
-    assert "allowed_actions" in result
-    assert "name" in result
+    assert "allowed_actions" in result, result
+    assert "name" in result, result
 
 
 def test_cli_action_list(cli: AACli) -> None:
     result = json.loads(cli("json", "action-list").stdout)
 
-    assert result == [] or "action_id" in result[0]
+    assert result == [] or "action_id" in result[0], result
 
 
 def test_cli_optional_option_in_help(cli: AACli) -> None:
     result = cli("json", "action-list", "--help")
 
-    assert "--status" in result.stdout
-    assert "[CANCELLED|FAILURE|PENDING|RUNNING|SUCCESS]" in result.stdout
-    assert "--action-user" in result.stdout
-    assert "--max-age-minutes" in result.stdout
+    assert "--status" in result.stdout, result.stdout
+    assert "[CANCELLED|" in result.stdout, result.stdout
+    assert "--action-user" in result.stdout, result.stdout
+    assert "--max-age-minutes" in result.stdout, result.stdout
 
 
 def test_cli_optional_option(cli: AACli) -> None:
@@ -66,8 +66,8 @@ def test_cli_optional_option(cli: AACli) -> None:
 def test_cli_required_option_in_help(cli: AACli) -> None:
     result = cli("json", "action-detail", "--help")
 
-    assert "--action-id" in result.stdout
-    assert "[required]" in result.stdout
+    assert "--action-id" in result.stdout, result.stdout
+    assert "[required]" in result.stdout, result.stdout
 
 
 def test_cli_required_option_not_given(cli: AACli) -> None:
@@ -75,7 +75,7 @@ def test_cli_required_option_not_given(cli: AACli) -> None:
         cli("json", "action-detail")
 
     assert e.value.returncode == 2  # noqa: PLR2004
-    assert "Missing option '--action-id'" in e.value.stderr
+    assert "Missing option '--action-id'" in e.value.stderr, e.value.stderr
 
 
 def test_cli_required_option(cli: AACli) -> None:
@@ -83,4 +83,4 @@ def test_cli_required_option(cli: AACli) -> None:
         cli("json", "action-detail", "--action-id", "this-id-does-not-exist")
 
     assert e.value.returncode == 1
-    assert '{"detail":"Item not found"}' in e.value.stderr
+    assert '{"detail":"Item not found"}' in e.value.stderr, e.value.stderr
