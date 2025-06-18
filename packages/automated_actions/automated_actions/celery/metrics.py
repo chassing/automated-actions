bc3f90e25ce7318a9d00d26e492aef5d569f03ab
Rafael Porres Molina <rporresm@redhat.com>
2025-06-18 18:32:56 +0200

    Redistribute action elapsed seconds bucket (#173)
    To better match the actual duration of the action processing.
    
    Signed-off-by: Rafa Porres Molina <rporresm@redhat.com>

--- packages/automated_actions/automated_actions/celery/metrics.py
+++ packages/automated_actions/automated_actions/celery/metrics.py
@@ -1,4 +1,5 @@
 from prometheus_client import CollectorRegistry, Histogram
+from prometheus_client.utils import INF
 
 CELERY_REGISTRY = CollectorRegistry()
 
@@ -7,4 +8,21 @@ action_elapsed_time = Histogram(
     documentation="Elapsed seconds since the moment in the action was inserted in the action table, including retries.",
     labelnames=["name", "status"],
     registry=CELERY_REGISTRY,
+    buckets=(
+        0.05,
+        0.075,
+        0.1,
+        0.33,
+        0.66,
+        1.0,
+        2.0,
+        4.0,
+        6.0,
+        8.0,
+        10.0,
+        15.0,
+        20.0,
+        30.0,
+        INF,
+    ),
 )
